4e578a475f2b2e5be5161e6968318a248d29e727
kwszeb2 <wszeborowski@sbcglobal.net>
2023-01-02 12:08:23 -0600

    Project using Django Framework

---
+++ mysite/mysite/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here
+
+def home(request):
+    return render(request, 'home.html')
+
+def attractions(request):
+    return render(request, 'attractions.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
