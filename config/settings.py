649723d44095ec953e93119ab15483873f829cb9
kwszeb2 <kwszeborowski@outlook.com>
2026-07-07 13:53:27 +0200

    update settings

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]  # global static folder
 
 # Media (user uploads)
